be2543ba265d027b2c6b5ed5199a0a0d02ae1ca9
Daiana Marasquin <daiana.marasquin@gmail.com>
2018-04-25 18:34:31 -0300

    First steps

---
+++ app/src/main/java/br/joinville/zarco/LoadingActivity.kt
@@ -0,0 +1,12 @@
+package br.joinville.zarco
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoadingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+    }
+}
---
+++ app/src/main/java/br/joinville/zarco/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.joinville.zarco
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+    }
+}
