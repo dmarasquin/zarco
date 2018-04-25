15d01ad7597a6fa95f9bc41945f356b6807442cc
Daiana Marasquin <daiana.marasquin@gmail.com>
2018-04-25 20:22:29 -0300

    Some actions between activities

--- app/src/main/java/br/joinville/zarco/MainActivity.kt
+++ app/src/main/java/br/joinville/zarco/MainActivity.kt
@@ -1,6 +1,8 @@
 package br.joinville.zarco
 
+import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +10,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newIssueButton = findViewById<FloatingActionButton>(R.id.new_issue_button)
+        newIssueButton.setOnClickListener({ startActivity(Intent(this, NewIssueActivity::class.java)) })
+    }
+
+    override fun onBackPressed() {
+        val closeIntent = Intent(Intent.ACTION_MAIN)
+        closeIntent.addCategory(Intent.CATEGORY_HOME)
+        startActivity(closeIntent)
     }
 }
--- app/src/main/java/br/joinville/zarco/NewIssueActivity.kt
+++ app/src/main/java/br/joinville/zarco/NewIssueActivity.kt
@@ -1,12 +1,17 @@
 package br.joinville.zarco
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 
 class NewIssueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_issue)
+
+        val saveButton = findViewById<Button>(R.id.save_button)
+        saveButton.setOnClickListener({ startActivity(Intent(this, MainActivity::class.java)) })
     }
 }
