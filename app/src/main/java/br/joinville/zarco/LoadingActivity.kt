d875cd72bc1049b9666d4da9cdad90e6b7e66406
Daiana Marasquin <daiana.marasquin@gmail.com>
2018-04-25 19:46:42 -0300

    Loading screen done

--- app/src/main/java/br/joinville/zarco/LoadingActivity.kt
+++ app/src/main/java/br/joinville/zarco/LoadingActivity.kt
@@ -1,12 +1,17 @@
 package br.joinville.zarco
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class LoadingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading)
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 3000)
     }
 }
--- app/src/main/java/br/joinville/zarco/NewIssueActivity.kt
+++ app/src/main/java/br/joinville/zarco/NewIssueActivity.kt
@@ -3,10 +3,10 @@ package br.joinville.zarco
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class NewIssueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_new_issue)
     }
 }
